230b4d11e92ab1076f6919b9841d313b93dab2a2
moshe <lerner1737@gmail.com>
2023-04-30 00:26:41 +0300

    menu layout

--- src/components/gamestart.jsx
+++ src/components/gamestart.jsx
@@ -6,9 +6,21 @@ export function Gamestart(){
         <>
             <div className="background">
                 <div className="level-container">
-                    <div className='level one'></div>
-                    <div className='level two'></div>
-                    <div className='level three'></div>
+                    <div className='level one'>
+                        <div className='text-background'>
+                            <p>Level 1</p>
+                        </div>
+                    </div>
+                    <div className='level two'>
+                        <div className='text-background'>
+                            <p>Level 2</p>
+                        </div>
+                    </div>
+                    <div className='level three'>
+                        <div className='text-background'>
+                            <p>Level 3</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
